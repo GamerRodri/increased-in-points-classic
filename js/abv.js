37902572a5118bc370665938508106419b8351ab
ming736 <80364687+ming736-2@users.noreply.github.com>
2023-06-12 15:07:14 -0400

    Redo abbreviation system, again

--- js/abv.js
+++ js/abv.js
@@ -1,8 +1,35 @@
-let abv = (value) => {
-  var length = (Math.abs(parseInt(value, 10)) + '').length,
-        index = Math.ceil((length - 3) / 3),
-        suffix = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','QnD'];
-    if (value < 1000 || length < 4) return value;
-    if (suffix[index - 1] == undefined) return value;
-    return (value / Math.pow(1000, index)).toFixed(1).replace(/\.0$/, '') + suffix[index - 1];
+let abv = (number, decPlaces) => {
+  if (!decPlaces) {decPlaces = 1}
+  // 2 decimal places => 100, 3 => 1000, etc
+  decPlaces = Math.pow(10, decPlaces)
+
+  // Enumerate number abbreviations
+  var abbrev = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD']
+
+  // Go through the array backwards, so we do the largest first
+  for (var i = abbrev.length - 1; i >= 0; i--) {
+    // Convert array index to "1000", "1000000", etc
+    var size = Math.pow(10, (i + 1) * 3)
+
+    // If the number is bigger or equal do the abbreviation
+    if (size <= number) {
+      // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+      // This gives us nice rounding to a particular decimal place.
+      number = Math.round((number * decPlaces) / size) / decPlaces
+
+      // Handle special case where we round up to the next abbreviation
+      if (number == 1000 && i < abbrev.length - 1) {
+        number = 1
+        i++
+      }
+
+      // Add the letter for the abbreviation
+      number += abbrev[i]
+
+      // We are done... stop
+      break
+    }
+  }
+
+  return number
 }
