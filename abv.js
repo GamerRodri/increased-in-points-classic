8bc50df8542148ccb2720a11b366e20952987603
ming736 <80364687+ming736-2@users.noreply.github.com>
2023-06-12 14:15:12 -0400

    Create abv.js

---
+++ abv.js
@@ -0,0 +1,16 @@
+let abv = (value) => {
+  var newValue = value;
+    if (value >= 1000) {
+        var suffixes = ["", "K", "M", "B","T","qd","Qn","Sx","Sp","O","N","de","UD","DD"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue = '';
+        for (var precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 2) { break; }
+        }
+        if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+        newValue = shortValue+suffixes[suffixNum];
+    }
+    return newValue;
+}
