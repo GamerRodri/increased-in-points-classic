383c762b7d65f8d8985ada0d892a90788ea6cf65
ming736 <80364687+ming736-2@users.noreply.github.com>
2023-06-12 14:46:25 -0400

    Update abv.js

--- js/abv.js
+++ js/abv.js
@@ -2,10 +2,8 @@ let abv = (value) => {
   var length = (Math.abs(parseInt(value, 10)) + '').length,
         index = Math.ceil((length - 3) / 3),
         suffix = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','QnD'];
-
+    console.log(length)
     if (length < 4) return value;
     
-    return (value / Math.pow(1000, index))
-           .toFixed(1)
-           .replace(/\.0$/, '') + suffix[index - 1];
+    return (value / Math.pow(1000, index)).toFixed(1).replace(/\.0$/, '') + suffix[index - 1];
 }
