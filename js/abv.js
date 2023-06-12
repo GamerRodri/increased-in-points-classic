157b259a3a183e1f801d2ed96c36053f5e38dbd3
ming736 <80364687+ming736-2@users.noreply.github.com>
2023-06-12 14:50:07 -0400

    Update abv.js to change abbreviation returning original value

--- js/abv.js
+++ js/abv.js
@@ -2,8 +2,7 @@ let abv = (value) => {
   var length = (Math.abs(parseInt(value, 10)) + '').length,
         index = Math.ceil((length - 3) / 3),
         suffix = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','QnD'];
-    console.log(length)
-    if (length < 4) return value;
+    if (value < 1000) return value;
     
     return (value / Math.pow(1000, index)).toFixed(1).replace(/\.0$/, '') + suffix[index - 1];
 }
