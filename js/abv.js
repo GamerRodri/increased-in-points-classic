429db053d832bc2da0a9868fa71c4f26a9a2fc44
ming736 <80364687+ming736-2@users.noreply.github.com>
2023-06-13 17:19:59 -0400

    Add more abbreviations

--- js/abv.js
+++ js/abv.js
@@ -4,7 +4,7 @@ let abv = (number, decPlaces) => {
   decPlaces = Math.pow(10, decPlaces)
 
   // Enumerate number abbreviations, now up to qtV!
-  var abbrev = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','Qnd','SxD','SpD','OcD','NvD','Vgn','UVg','DVg','qtV']
+  var abbrev = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','Qnd','SxD','SpD','OcD','NvD','Vgn','UVg','DVg','qtV','QnV','SeV','SPG','OVG','NVG','TGN','UTG','DTG','tsTG','qtTG','QnTG']
   if (number >= Math.pow(10,abbrev.length)) {
     return number
   }
