2664a02a939d60bc6527c5485b710e536379aff5
ming736 <80364687+ming736-2@users.noreply.github.com>
2023-06-13 19:55:23 -0400

    Add abbreviation check

--- js/abv.js
+++ js/abv.js
@@ -1,12 +1,13 @@
+let abbrev = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','Qnd','SxD','SpD','OcD','NvD','Vgn','UVg','DVg','qtV','QnV','SeV','SPG','OVG','NVG','TGN','UTG','DTG','tsTG','qtTG','QnTG']
+console.log(`Abbreviations should be able to go up to ${Math.pow(10,(abbrev.length*3)+3)}`)
 let abv = (number, decPlaces) => {
   if (!decPlaces) {decPlaces = 3}
   // 2 decimal places => 100, 3 => 1000, etc
   decPlaces = Math.pow(10, decPlaces)
 
   // Enumerate number abbreviations, now up to qtV!
-  var abbrev = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','Qnd','SxD','SpD','OcD','NvD','Vgn','UVg','DVg','qtV','QnV','SeV','SPG','OVG','NVG','TGN','UTG','DTG','tsTG','qtTG','QnTG']
   if (number >= Math.pow(10,(abbrev.length*3)+3)) {
-    return new Number(number).toFixed(1)
+    return new Number(number).toFixed(decPlaces)
   }
   // Go through the array backwards, so we do the largest first
   for (var i = abbrev.length - 1; i >= 0; i--) {
