99dec12e2eb6983776239891b6f3a9f5887beecb
RodriGamer <80364792+GamerRodri@users.noreply.github.com>
2023-07-18 09:03:25 -0400

    Update abv.js

--- js/abv.js
+++ js/abv.js
@@ -1,6 +1,8 @@
 let abbrev = ['K', 'M', 'B', 'T','qd','Qn','Sx','Sp','O','N','de','Ud','DD','tdD','qdD','Qnd','SxD','SpD','OcD','NvD','Vgn','UVg','DVg','qtV','QnV','SeV','SPG','OVG','NVG','TGN','UTG','DTG','tsTG','qtTG','QnTG','ssTG','SpTG','OcTG','NoTG','QdDR','uQDR','dQDR','tQDR','qdQDR','QnQDR','SxQDR','SpQDR','OQDDR','NQDDR','QGNT','uQGNT','dQGNT','tQGNT','qdQGNT','QnQGNT','SxQGNT','SpQGNT','OQQGNT','NQQGNT','SXGNTL','uSXGNTL','dSXGNTL','tSXGNTL','qdSXGNTL','QnSXGNTL','SxSXGNTL','SpSXGNTL','OSXGNTL','NvSXGNTL','SPTGNTL','uSPTGNTL','dSPTGNTL','tSPTGNTL','qdSPTGNTL','QnSPTGNTL','SxSPTGNTL','SpSPTGNTL','OSPTGNTL','NvSPTGNTL','OTGNTL','uOTGNTL','dOTGNTL','tOTGNTL','qdOTGNTL','QnOTGNTL','SxOTGNTL','SpOTGNTL','OOTGNTL','NvOTGNTL','NONGNTL','uNONGNTL','dNONGNTL','tNONGNTL','qdNONGNTL','QnNONGNTL','SxNONGNTL','SpNONGNTL','ONONGNTL','NvNONGNTL','CENT','uCENT','dCENT','tCENT','qdCENT']
-console.log(`Abbreviations should be able to go up to ${Math.pow(10,(abbrev.length*3)+3)}`)
-let abv = (number, decPlaces) => {
+let abv = (number, startAbv, decPlaces) => {
+  if (startAbv == null || startAbv >= 21) {
+    startAbv = 21
+  }
   let orNum = number
   if (!decPlaces) {decPlaces = 3}
   // 2 decimal places => 100, 3 => 1000, etc
@@ -17,8 +19,12 @@ let abv = (number, decPlaces) => {
   }
   // Go through the array backwards, so we do the largest first
   for (var i = abbrev.length - 1; i >= 0; i--) {
-    // Convert array index to "1000", "1000000", etc
-    var size = Math.pow(10, (i + 1) * 3)
+    if (number >= Math.pow(10, startAbv)) {
+      // Convert array index to "1000", "1000000", etc
+      var size = Math.pow(10, (i + 1) * 3)
+    } else {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
 
     // If the number is bigger or equal do the abbreviation
     if (size <= number) {
